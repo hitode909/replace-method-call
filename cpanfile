0f660f6469b30bc4a24583b18e26c93db4fe2e9a
hitode909 <hitode909@gmail.com>
2013-09-25 19:00:58 +0900

    test methods

--- cpanfile
+++ cpanfile
@@ -8,3 +8,4 @@ requires 'Class::Accessor::Lite';
 test_requires 'Path::Class';
 test_requires 'Test::More';
 test_requires 'Test::Class';
+test_requires 'Exporter::Lite';
--- t/test.pm
+++ t/test.pm
@@ -4,8 +4,17 @@ use strict;
 use warnings;
 use utf8;
 
+use PPI;
 use Path::Class;
 use lib file(__FILE__)->dir->parent->subdir('lib')->stringify;
+
+use Exporter::Lite ();
+our @EXPORT = qw(
+                    test_file
+                    doc_from_file
+                    doc_from_content
+);
+
 
 sub import {
     my ($class) = @_;
@@ -26,7 +35,23 @@ sub import {
 
     eval $code;
     die $@ if $@;
+    goto &Exporter::Lite::import;
+}
 
+sub test_file {
+    my ($file_name) = @_;
+
+    file(__FILE__)->dir->subdir('data')->file($file_name);
+}
+
+sub doc_from_file {
+    my ($file_name) = @_;
+    PPI::Document->new(test_file($file_name).q());
+}
+
+sub doc_from_content {
+    my ($content) = @_;
+    PPI::Document->new(\$content);
 }
 
 1;
